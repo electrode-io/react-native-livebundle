565cf6f0be898c25cc303c40e5980e7ddd11dc6e
Benoit Lemaire <lemaireb@gmail.com>
2020-09-30 15:27:28 -0700

    Rename Android package

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -1,4 +1,4 @@
-package com.livebundle.example;
+package io.livebundle.example;
 
 import android.app.Application;
 import android.content.Context;
@@ -10,8 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.livebundle.LiveBundleModule;
-import com.livebundle.example.MainActivity;
+import io.livebundle.LiveBundleModule;
+import io.livebundle.example.MainActivity;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -68,7 +68,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.livebundle.example.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("io.livebundle.example.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
