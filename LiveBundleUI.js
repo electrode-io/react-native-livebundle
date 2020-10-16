afa9428be95b1d12c28ac73afb21004ddb88335e
Benoit Lemaire <lemaireb@gmail.com>
2020-10-16 13:39:11 -0700

    Refactor LiveBundleUI class

--- LiveBundleUI.js
+++ LiveBundleUI.js
@@ -12,6 +12,121 @@ import {
 import { RNCamera } from "react-native-camera";
 import livebundle from "./LiveBundle";
 
+const ErrorScreen = ({ errorMessage }) => (
+  <View style={styles.subContainer}>
+    <Image
+      resizeMode="contain"
+      style={styles.logo}
+      source={require("./assets/logo.png")}
+    />
+    <Text style={styles.error}>{errorMessage}</Text>
+  </View>
+);
+
+const LoadingScreen = () => (
+  <View style={styles.subContainer}>
+    <Image
+      resizeMode="contain"
+      style={styles.logo}
+      source={require("./assets/logo.png")}
+    />
+    <Text style={styles.bottomText}>Loading Package</Text>
+  </View>
+);
+
+const BundleFlavorSelectionScreen = ({ platformBundles, onBundleSelected }) => (
+  <View style={styles.subContainer}>
+    <Image
+      resizeMode="contain"
+      style={styles.logo}
+      source={require("./assets/logo.png")}
+    />
+    <Text style={styles.text}>Select bundle flavor to install</Text>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => {
+        onBundleSelected(platformBundles.find((b) => b.dev).id);
+      }}
+    >
+      <Text style={styles.buttonText}>Dev</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => {
+        onBundleSelected(platformBundles.find((b) => !b.dev).id);
+      }}
+    >
+      <Text style={styles.buttonText}>Prod</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+const QRCodeScannerScreen = ({ onQrCodeRead }) => (
+  <RNCamera
+    captureAudio={false}
+    style={styles.preview}
+    type={RNCamera.Constants.Type.back}
+    flashMode={RNCamera.Constants.FlashMode.off}
+    androidCameraPermissionOptions={{
+      title: "Permission to use camera",
+      message: "We need your permission to use your camera",
+      buttonPositive: "Ok",
+      buttonNegative: "Cancel",
+    }}
+    onBarCodeRead={({ data }) => {
+      if (data.startsWith("s:")) {
+        onQrCodeRead({ sessionId: data.replace("s:", "") });
+      } else {
+        onQrCodeRead({ packageId: data });
+      }
+    }}
+  />
+);
+
+const MainScreen = ({
+  bundleId,
+  packageId,
+  isBundleInstalled,
+  isSessionStarted,
+  onScanInitiated,
+}) => (
+  <View style={styles.subContainer}>
+    <Image
+      resizeMode="contain"
+      style={styles.logo}
+      source={require("./assets/logo.png")}
+    />
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => {
+        onScanInitiated();
+      }}
+    >
+      <Text style={styles.buttonText}>Scan</Text>
+    </TouchableOpacity>
+    {(isBundleInstalled || isSessionStarted) && (
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          livebundle.reset();
+          BackHandler.exitApp();
+        }}
+      >
+        <Text style={styles.buttonText}>Reset</Text>
+      </TouchableOpacity>
+    )}
+    {isBundleInstalled && (
+      <Text style={styles.bottomText}>{`packageId: ${packageId}`}</Text>
+    )}
+    {isBundleInstalled && (
+      <Text style={styles.bottomText2}>{`bundleId: ${bundleId}`}</Text>
+    )}
+    {isSessionStarted && (
+      <Text style={styles.bottomText2}>{`Connected to live session`}</Text>
+    )}
+  </View>
+);
+
 export class LiveBundleUI extends Component<{}> {
   constructor(props) {
     super(props);
@@ -38,16 +153,7 @@ export class LiveBundleUI extends Component<{}> {
     } = this.state;
     let screen;
     if (error) {
-      screen = (
-        <View style={styles.subContainer}>
-          <Image
-            resizeMode="contain"
-            style={styles.logo}
-            source={require("./assets/logo.png")}
-          />
-          <Text style={styles.error}>{error.message}</Text>
-        </View>
-      );
+      screen = <ErrorScreen errorMessage={error.message} />;
     } else if (isDownloadCompleted) {
       // If we have a bundle ready to be installed, install
       // it and exist the menu
@@ -56,7 +162,6 @@ export class LiveBundleUI extends Component<{}> {
       });
       BackHandler.exitApp();
     } else if (sessionId) {
-      console.log(`sessionId: ${sessionId}`);
       // If we have a session id just launch the live session
       livebundle.launchLiveSession(sessionId).catch((error) => {
         this.setState({ error });
@@ -66,16 +171,7 @@ export class LiveBundleUI extends Component<{}> {
       // If we have a packageId but no bundleId yet then we need
       // to retrieve the package metadata to look at what bundles
       // the package contains
-      screen = (
-        <View style={styles.subContainer}>
-          <Image
-            resizeMode="contain"
-            style={styles.logo}
-            source={require("./assets/logo.png")}
-          />
-          <Text style={styles.bottomText}>Loading Package</Text>
-        </View>
-      );
+      screen = <LoadingScreen />;
       livebundle
         .getPackageMetadata(packageId)
         .then((packageMetadata) => {
@@ -102,34 +198,10 @@ export class LiveBundleUI extends Component<{}> {
         // More than one bundle in package for this platform.
         // Ask user to choose bundle flavor -for now-.
         screen = (
-          <View style={styles.subContainer}>
-            <Image
-              resizeMode="contain"
-              style={styles.logo}
-              source={require("./assets/logo.png")}
-            />
-            <Text style={styles.text}>Select bundle flavor to install</Text>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                this.setState({
-                  bundleId: platformBundles.find((b) => b.dev).id,
-                });
-              }}
-            >
-              <Text style={styles.buttonText}>Dev</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                this.setState({
-                  bundleId: platformBundles.find((b) => !b.dev).id,
-                });
-              }}
-            >
-              <Text style={styles.buttonText}>Prod</Text>
-            </TouchableOpacity>
-          </View>
+          <BundleFlavorSelectionScreen
+            platformBundles={platformBundles}
+            onBundleSelected={(bundleId) => this.setState({ bundleId })}
+          />
         );
       }
     } else if (packageId && bundleId) {
@@ -146,34 +218,14 @@ export class LiveBundleUI extends Component<{}> {
     } else if (isScanInitiated) {
       // If the scanning has been triggered, show the scanner
       screen = (
-        <RNCamera
-          ref={(ref) => {
-            this.camera = ref;
-          }}
-          captureAudio={false}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.off}
-          androidCameraPermissionOptions={{
-            title: "Permission to use camera",
-            message: "We need your permission to use your camera",
-            buttonPositive: "Ok",
-            buttonNegative: "Cancel",
-          }}
-          onBarCodeRead={({ data }) => {
-            if (data.startsWith("s:")) {
-              this.setState({
-                isScanInitiated: false,
-                sessionId: data.replace("s:", ""),
-              });
-            } else {
-              this.setState({ isScanInitiated: false, packageId: data });
-            }
-          }}
+        <QRCodeScannerScreen
+          onQrCodeRead={({ packageId, sessionId }) =>
+            this.setState({ isScanInitiated: false, packageId, sessionId })
+          }
         />
       );
     } else {
-      // LiveBundle menu screen
+      // LiveBundle main screen
       const {
         IS_BUNDLE_INSTALLED,
         IS_SESSION_STARTED,
@@ -181,43 +233,13 @@ export class LiveBundleUI extends Component<{}> {
         BUNDLE_ID,
       } = livebundle;
       screen = (
-        <View style={styles.subContainer}>
-          <Image
-            resizeMode="contain"
-            style={styles.logo}
-            source={require("./assets/logo.png")}
-          />
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              this.setState({ isScanInitiated: true });
-            }}
-          >
-            <Text style={styles.buttonText}>Scan</Text>
-          </TouchableOpacity>
-          {(IS_BUNDLE_INSTALLED || IS_SESSION_STARTED) && (
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                livebundle.reset();
-                BackHandler.exitApp();
-              }}
-            >
-              <Text style={styles.buttonText}>Reset</Text>
-            </TouchableOpacity>
-          )}
-          {IS_BUNDLE_INSTALLED && (
-            <Text style={styles.bottomText}>{`packageId: ${PACKAGE_ID}`}</Text>
-          )}
-          {IS_BUNDLE_INSTALLED && (
-            <Text style={styles.bottomText2}>{`bundleId: ${BUNDLE_ID}`}</Text>
-          )}
-          {IS_SESSION_STARTED && (
-            <Text
-              style={styles.bottomText2}
-            >{`Connected to live session`}</Text>
-          )}
-        </View>
+        <MainScreen
+          bundleId={BUNDLE_ID}
+          packageId={PACKAGE_ID}
+          isBundleInstalled={IS_BUNDLE_INSTALLED}
+          isSessionStarted={IS_SESSION_STARTED}
+          onScanInitiated={() => this.setState({ isScanInitiated: true })}
+        />
       );
     }
 
